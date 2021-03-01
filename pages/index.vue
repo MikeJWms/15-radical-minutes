baf9fe4ded7e601c0ec8046f50033bf89f621446
Michael Williams <michaeljwms@live.com>
2021-02-28 18:44:12 -0800

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">15-radical-minutes</h1>
+      <h2 class="subtitle">With Michael Williams & Cody McGraw</h2>
+      <p>Join us on Clubhouse for 15 radical minutes with some of today's most inspiring thought leaders, builders, and innovators.</p>
+      <br>
+      <p>Follow @15radicalminutes on twitter to see when we're live next!</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
