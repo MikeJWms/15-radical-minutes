cc64b0fc8fdf9c568d1e4e0842cd7c3cc4cb237f
Michael Williams <michaeljwms@live.com>
2021-03-01 17:08:43 -0800

    added 'previous guests section'

---
+++ components/PreviousGuests.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="bg-indigo-800 px-4 rounded-lg mb-6 mx-6">
+      <h2 class="text-white text-center pt-9 pb-0 text-2xl font-normal md:font-semibold md:text-3xl">Our previous guests:</h2>
+      <div class="flex flex-wrap w-full justify-center py-5">
+        <div class="personality sm:w-full md:w-40">
+          <img src="@/assets/Nadya_Okamoto_400x400.jpg" alt="" />
+          <p>Nadya Okamoto</p>
+          <span class="text-gray-200 font-light">Entrepreneur</span>
+        </div>
+
+        <div class="personality sm:w-full md:w-40">
+          <img src="@/assets/Ana_Chaud_400x400.jpg" alt="" />
+          <p>Ana Chaud</p>
+          <span class="text-gray-200 font-light">Entrepreneur</span>
+        </div>
+
+        <div class="personality sm:w-full md:w-40">
+          <img src="@/assets/Skip_Newberry_400x400.jpg" alt="" />
+          <p>Skip Newberry</p>
+          <span class="text-gray-200 font-light">CEO @ TAO</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+img {
+  clip-path: circle(50% at 50% 50%);
+  max-width: 100%;
+  @apply mx-auto;
+  @apply mb-4;
+}
+
+.personality {
+  @apply m-10;
+  @apply text-center;
+  @apply text-white;
+  @apply flex-row-reverse;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,21 @@
 <template>
-  <div class="container">
-    <div>
-      <h1 class="title">15-radical-minutes</h1>
-      <h2 class="subtitle">With Michael Williams & Cody McGraw</h2>
-      <p>Join us on Clubhouse for 15 radical minutes with some of today's most inspiring thought leaders, builders, and innovators.</p>
-      <br>
-      <p>Follow @15radicalminutes on twitter to see when we're live next!</p>
+  <div>
+    <div class="container">
+      <div>
+        <h1 class="text-indigo-900 text-6xl font-bold my-5">
+          15 Radical Minutes
+        </h1>
+        <p class="text-gray-600 text-xl">with</p>
+        <h2 class="text-gray-600 text-4xl my-2 mb-14">Michael & Cody</h2>
+        <p>
+          Join us on Clubhouse for 15 radical minutes with some of today's most
+          inspiring thought leaders, builders, and innovators.
+        </p>
+        <br />
+        <p>Follow <a href="https://twitter.com/15radicalmins" class="text-blue-500">@15radicalminutes</a> on twitter to see when we're live next!</p>
+      </div>
     </div>
+    <PreviousGuests class="max-w-6xl mx-auto" />
   </div>
 </template>
 
@@ -24,11 +33,13 @@ export default Vue.extend({})
 */
 .container {
   margin: 0 auto;
-  min-height: 100vh;
+  min-height: 70vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
+  @apply px-4;
+  @apply max-w-6xl;
 }
 
 .title {
