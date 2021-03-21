5e6cb81065f61886c4d498a6a94d29ee3b163a18
Michael Williams <michaeljwms@live.com>
2021-03-21 13:53:31 -0700

    moved guests to their own components. Made very slight style imporvements to guest component.

---
+++ components/Guest.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="personality sm:w-full md:w-40">
+    <img
+      :src="image"
+      alt=""
+      class="shadow-lg rounded-full max-w-full h-auto align-middle border-none"
+    />
+    <p class="text-xl font-semibold mt-3">{{ name }}</p>
+    <span class="text-gray-200 font-light">{{ title }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    image: {
+      type: String,
+      default: null,
+    },
+    name: {
+      type: String,
+      default: 'Null',
+    },
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+})
+</script>
+
+
+<style scoped>
+.personality {
+  @apply m-10;
+  @apply text-center;
+  @apply text-white;
+  @apply flex-row-reverse;
+}
+</style>
--- components/PreviousGuests.vue
+++ components/PreviousGuests.vue
@@ -1,42 +1,52 @@
 <template>
   <div>
     <div class="bg-indigo-800 px-4 rounded-lg mb-6 mx-6">
-      <h2 class="text-white text-center pt-9 pb-0 text-2xl font-normal md:font-semibold md:text-3xl">Our previous guests:</h2>
+      <h2
+        class="text-white text-center pt-9 pb-0 text-2xl font-normal md:font-semibold md:text-3xl"
+      >
+        Our previous guests:
+      </h2>
       <div class="flex flex-wrap w-full justify-center py-5">
-        <div class="personality sm:w-full md:w-40">
-          <img src="@/assets/Nadya_Okamoto_400x400.jpg" alt="" />
-          <p>Nadya Okamoto</p>
-          <span class="text-gray-200 font-light">Entrepreneur</span>
-        </div>
-
-        <div class="personality sm:w-full md:w-40">
-          <img src="@/assets/Ana_Chaud_400x400.jpg" alt="" />
-          <p>Ana Chaud</p>
-          <span class="text-gray-200 font-light">Entrepreneur</span>
-        </div>
-
-        <div class="personality sm:w-full md:w-40">
-          <img src="@/assets/Skip_Newberry_400x400.jpg" alt="" />
-          <p>Skip Newberry</p>
-          <span class="text-gray-200 font-light">CEO @ TAO</span>
+        <div v-for="previousGuest in previousGuests" :key="previousGuest.name">
+          <Guest
+            :image="previousGuest.picturePath"
+            :name="previousGuest.name"
+            :title="previousGuest.title"
+          />
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<style scoped>
-img {
-  clip-path: circle(50% at 50% 50%);
-  max-width: 100%;
-  @apply mx-auto;
-  @apply mb-4;
-}
-
-.personality {
-  @apply m-10;
-  @apply text-center;
-  @apply text-white;
-  @apply flex-row-reverse;
-}
-</style>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      previousGuests: [
+        {
+          picturePath: require('@/assets/sam_adams_400x400.jpg'),
+          name: 'Sam Adams',
+          title: 'Former Mayor, Portland, OR',
+        },
+        {
+          picturePath: require('@/assets/Nadya_Okamoto_400x400.jpg'),
+          name: 'Nadya Okamoto',
+          title: 'Social Entrepreneur & Author',
+        },
+        {
+          picturePath: require('@/assets/Ana_Chaud_400x400.jpg'),
+          name: 'Ana Chaud',
+          title: 'Food Entrepreneur & Startup Stratagist',
+        },
+        {
+          picturePath: require('@/assets/Skip_Newberry_400x400.jpg'),
+          name: 'Skip Newberry',
+          title: 'CEO, Technology Association of Oregon',
+        },
+      ],
+    }
+  },
+})
+</script>
